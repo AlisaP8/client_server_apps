09edbcb9a58167217b4e69c30b48b430fa5ce34f
AlisaP8 <78349377+AlisaP8@users.noreply.github.com>
2022-03-22 01:00:01 +0400

    lesson_3

---
+++ lesson_3/client.py
@@ -0,0 +1,58 @@
+import sys
+import json
+import socket
+import time
+from common.variables import ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, \
+    RESPONSE, ERROR, DEFAULT_IP_ADDRESS, DEFAULT_PORT
+from common.utils import get_message, send_message
+
+
+def create_presence(account_name='Guest'):
+
+    out = {
+        ACTION: PRESENCE,
+        TIME: time.time(),
+        USER: {
+            ACCOUNT_NAME: account_name
+        }
+    }
+    return out
+
+
+def process_ans(message):
+
+    if RESPONSE in message:
+        if message[RESPONSE] == 200:
+            return '200 : OK'
+        return f'400 : {message[ERROR]}'
+    raise ValueError
+
+
+def main():
+
+    try:
+        server_address = sys.argv[1]
+        server_port = int(sys.argv[2])
+        if not 65535 >= server_port >= 1024:
+            raise ValueError
+    except IndexError:
+        server_address = DEFAULT_IP_ADDRESS
+        server_port = DEFAULT_PORT
+    except ValueError:
+        print('Порт должен быть указан в пределах от 1024 до 65535.')
+        sys.exit(1)
+
+    transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    transport.connect((server_address, server_port))
+
+    message_to_server = create_presence()
+    send_message(transport, message_to_server)
+    try:
+        answer = process_ans(get_message(transport))
+        print(answer)
+    except (ValueError, json.JSONDecodeError):
+        print('Не удалось декодировать сообщение сервера.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ lesson_3/common/variables.py
@@ -0,0 +1,15 @@
+DEFAULT_PORT = 7777
+DEFAULT_IP_ADDRESS = '127.0.0.1'
+MAX_CONNECTIONS = 5
+MAX_PACKAGE_LENGTH = 1024
+ENCODING = 'utf-8'
+
+ACTION = 'action'
+TIME = 'time'
+USER = 'user'
+ACCOUNT_NAME = 'account_name'
+
+PRESENCE = 'presence'
+RESPONSE = 'response'
+ERROR = 'error'
+RESPONDEFAULT_IP_ADDRESS = 'respondefault_ip_address'
